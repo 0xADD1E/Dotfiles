9faf97d4ba920430fce2c05a874a6944a34d78b9
Addie Morrison <developer@0xADD1E.me>
2019-09-03 20:06:08 -0500

    Add macOS user python path, use fish test

--- config/fish/conf.d/00_path.fish
+++ config/fish/conf.d/00_path.fish
@@ -1,12 +1,15 @@
-if stat $HOME/bin > /dev/null
+if test -d $HOME/bin
 	set -gx PATH $HOME/bin $PATH
 end
-if stat $HOME/.cargo/bin > /dev/null
+if test -d $HOME/.cargo/bin
 	set -gx PATH $HOME/.cargo/bin $PATH
 end
-if stat $HOME/.yarn/bin > /dev/null
+if test -d $HOME/.yarn/bin
 	set -gx PATH $HOME/.yarn/bin $PATH
 end
-if stat $HOME/.local/bin > /dev/null
+if test -d $HOME/.local/bin
 	set -gx PATH $HOME/.local/bin $PATH
+end
+if test -d $HOME/Library/Python/2.7/bin
+	set -gx PATH $HOME/Library/Python/2.7/bin $PATH
 end
