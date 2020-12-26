b1c0bcca2f761d16271491cdf619b5e4c1a1c549
Kája Lišková <developer@0xADD1E.me>
2020-12-26 12:22:58 -0600

    Cleanup fish init

--- config/fish/conf.d/00_path.fish
+++ config/fish/conf.d/00_path.fish
@@ -1,18 +1,14 @@
-if test -d $HOME/bin
-	set -gx PATH $HOME/bin $PATH
+function _prepend_if_exists
+    if test -d $1
+        set -gx fish_user_paths $1 $fish_user_paths
+    end
 end
-if test -d $HOME/.cargo/bin
-	set -gx PATH $HOME/.cargo/bin $PATH
-end
-if test -d $HOME/.yarn/bin
-	set -gx PATH $HOME/.yarn/bin $PATH
-end
-if test -d $HOME/.local/bin
-	set -gx PATH $HOME/.local/bin $PATH
-end
-if test -d $HOME/Library/Python/2.7/bin
-	set -gx PATH $HOME/Library/Python/2.7/bin $PATH
-end
-if test -d $HOME/Library/Python/3.7/bin
-	set -gx PATH $HOME/Library/Python/3.7/bin $PATH
-end
+_prepend_if_exists /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin
+_prepend_if_exists $HOME/bin
+_prepend_if_exists $HOME/.cargo/bin
+_prepend_if_exists $HOME/.yarn/bin
+_prepend_if_exists $HOME/.local/bin
+_prepend_if_exists $HOME/Library/Python/2.7/bin
+_prepend_if_exists $HOME/Library/Python/3.7/bin
+_prepend_if_exists /opt/homebrew/bin
+_prepend_if_exists /opt/homebrew/sbin
--- config/fish/conf.d/02_colours.fish
+++
@@ -1,3 +0,0 @@
-if test -d $HOME/Documents/Code/base16-builder-rust
-	source $HOME/Documents/Code/base16-builder-rust/templates/shell/profile_helper.fish
-end
